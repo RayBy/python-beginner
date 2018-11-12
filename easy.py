f73f167f11ad03904947fc09d4cfb2eaf080d280
ILINYKH PAVEL <ili.pas@outlook.com>
2018-11-12 12:04:25 +0200

    оформление действий в easy в виде функций

--- easy.py
+++ easy.py
@@ -11,29 +11,38 @@ import shutil
 # И второй скрипт, удаляющий эти папки.
 
 # Создание папок dir_ c 1 по 9
-i = 1
-while i <= 9:
-    d = 'dir_' + str(i)
-    os.mkdir(d)
-    i += 1
+def create_dir():
+    i = 1
+    while i <= 9:
+        d = 'dir_' + str(i)
+        os.mkdir(d)
+        i += 1
+
+create_dir()
 
 # Удаление папок из текущей диретории имя которых начинается с dir_
-file_list = os.listdir()
-i_2 = 0
-while i_2 < len(file_list):
-    name = os.path.join(file_list[i_2])
-    if name.startswith('dir_'):
-        os.rmdir(name)
-    i_2 += 1
+def del_dir():
+    file_list = os.listdir()
+    i_2 = 0
+    while i_2 < len(file_list):
+        name = os.path.join(file_list[i_2])
+        if name.startswith('dir_'):
+            os.rmdir(name)
+        i_2 += 1
+
+del_dir()
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+def file_list():
+    print(os.listdir())
 
-print(os.listdir())
-
+file_list()
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+def copy_file():
+    new_file = __file__ + '.copy'
+    shutil.copy(__file__, new_file)
 
-new_file = __file__ + '.copy'
-shutil.copy(__file__, new_file)
+copy_file()
